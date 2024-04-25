d30c3e13adf22007503ce16f2b6c7f2cc370abe9
Steven lopez <cslopezmontano@gmail.com>
2024-04-25 16:13:54 -0500

    Mensaje de christian

--- src/test/java/qualityStreamTutorial/mercuryAutomatedTest.java
+++ src/test/java/qualityStreamTutorial/mercuryAutomatedTest.java
@@ -5,21 +5,25 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
 
 public class mercuryAutomatedTest {
 
 	//Encontrar el chromeDriver
 	String chromeDriverResourse = "./src/test/resources/chromeDriver/chromedriver.exe";
 	//Instanciar el webDriver
-	private webDriver driver;
+	private WebDriver driver;
 	
 	//Before es para la preparacion de la pagina y demas
 	@Before
 	public void setUp() throws Exception {
-		System.setProperty("webdriver.chrome.driver", chromeDriverResourse)
-		
+		System.setProperty("webdriver.chrome.driver", chromeDriverResourse);
+		//Maximizamos la ventana
+		driver.manage().window().maximize();
+		//URL de la pagina a automatizar
+		driver.get("https://demo.guru99.com/test/newtours/index.php");
 	}
-
+ //El viejo Christian nos volvera automatizadores
 	@After
 	public void tearDown() throws Exception {
 	}
